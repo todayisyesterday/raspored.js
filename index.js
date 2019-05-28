7611f9b350e135618e4c6c04f64c61ad89fe1542
todayisyesterday <todayisy0@gmail.com>
2019-05-28 21:02:34 +0200

    third commit

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question('Unesite link:\t', (answer) => {
+rl.question('Unesite link:    ', (answer) => {
   console.log(`Uneli ste:\t${answer}`);
-  let dir = './raspored'+answer.substring('http://raspored.rs/pub/?pid='.length);
+  let dir = './raspored-'+answer.substring('http://raspored.rs/pub/?pid='.length);
   let options = {
       urls: [answer],
       directory: dir,
   };
   scrape(options).then((result) => {
-      console.log("Uspesno!");
+      console.log(`Uspesno! Sacuvano u direktorijumu ${dir}`);
   }).catch((err) => {
       console.log("ups", err);
   });
